04cba32afebf9086850bf534ef992275edcec826
pixelypants <hudson.graham@gmail.com>
2019-12-30 18:00:59 +1000

    Add ObservableStore to root app

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 import * as isActive from './activityFns.js'
 import * as singleSpa from 'single-spa'
+import { ObservableStore } from '@codewithdan/observable-store';
+import { ReduxDevToolsExtension } from '@codewithdan/observable-store-extensions';
 
 singleSpa.registerApplication('navbar', () => SystemJS.import('@portal/navbar'), isActive.navbar)
 singleSpa.registerApplication('sports', () => SystemJS.import('@portal/sports'), isActive.sports)
+
+ObservableStore.addExtension(new ReduxDevToolsExtension({ reactRouterHistory: history }))
 
 singleSpa.start()
