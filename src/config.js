5036c50ecd5c03e31d4102976d01411f381f808c
pixelypants <hudson.graham@gmail.com>
2020-01-01 13:20:20 +1000

    Add import map override for DX

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ import * as singleSpa from 'single-spa'
 import { ObservableStore } from '@codewithdan/observable-store';
 import { ReduxDevToolsExtension } from '@codewithdan/observable-store-extensions';
 
+ObservableStore.addExtension(new ReduxDevToolsExtension({ reactRouterHistory: history }))
+
 singleSpa.registerApplication('navbar', () => SystemJS.import('@portal/navbar'), isActive.navbar)
 singleSpa.registerApplication('sports', () => SystemJS.import('@portal/sports'), isActive.sports)
 
